f9b5584c0633fe87806ca70d342da77099341309
xmark2 <xmark2@gmail.com>
2022-10-31 17:11:08 +0100

    box layout

---
+++ exp/08app.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+
+Builder.load_file('box.kv')
+
+
+class MyLayout(Widget):
+    pass
+
+
+class AwesomeApp(App):
+    def build(self):
+        return MyLayout()
+
+
+if __name__ == "__main__":
+    AwesomeApp().run()
